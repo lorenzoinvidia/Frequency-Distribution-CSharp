022082e5b0a484544cca7a2998201498087abc8a
Lorenzo Invidia <loreinvidia@gmail.com>
2018-10-08 17:30:00 +0200

    comm001

---
+++ Application2/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Application2 {
+
+    public class Person {
+        public int Age { get; set; }
+        public char Gender { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+    }
+}
---
+++ Application2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            List<Person> people = new List<Person> {
+                new Person(){ Age=21, Gender='M', Weight=64.5, Height=170 },
+                new Person(){ Age=25, Gender='F', Weight=64.5, Height=170 },
+            };
+
+           
+            foreach (Person p in people)
+                System.Console.WriteLine("Age:" + p.Age + " " + "Weight:" + p.Weight);
+
+
+            Application.Run(new Form1());
+        }
+    }
+}
